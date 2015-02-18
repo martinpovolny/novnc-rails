138d4c3c47797f5614b546c757aa3ed10c40d2f1
Martin Povolny <mpovolny@redhat.com>
2015-02-18 13:27:15 +0100

    Add javascript assets to novnc-rails.js.

---
+++ vendor/assets/javascripts/novnc-rails.js
@@ -0,0 +1,27 @@
+// require noVNC/Orbitron700.ttf
+// require noVNC/Orbitron700.woff
+// require noVNC/base.css
+// require noVNC/black.css
+// require noVNC/blue.css
+
+//= require noVNC/base64
+//= require noVNC/des
+//= require noVNC/util
+//= require noVNC/input
+//= require noVNC/jsunzip
+//= require noVNC/display
+//= require noVNC/keyboard
+//= require noVNC/keysym
+//= require noVNC/keysymdef
+//= require noVNC/logo
+//= require noVNC/playback
+//= require noVNC/rfb
+// require noVNC/ui
+//= require noVNC/websock
+//= require noVNC/webutil
+// require noVNC/web-socket-js
+// require noVNC/web-socket-js/README.txt
+// require noVNC/web-socket-js/WebSocketMain.swf
+//= require noVNC/web-socket-js/swfobject.js
+//= require noVNC/web-socket-js/web_socket.js
+
